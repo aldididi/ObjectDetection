956fb2aa9c7669f6a7636e0485349c6ec929c724
aldididi <aldyh024@gmail.com>
2022-06-18 13:04:24 +0700

    add sweetalert

--- static/index-old.js
+++ static/index-old.js
@@ -21,21 +21,8 @@ window.onload = () => {
           console.log(data);
           // bytestring = data["status"];
           // image = bytestring.split("'")[1];
-          Swal.fire({
-            title: 'Berhasil',
-            text: "File berhasil dideteksi",
-            type: 'success',
-          }).then((result) => {
-              if (result.value) {
-                return false;
-              }
-          });
-          
           $("#link").css("visibility", "visible");
           $("#download").attr("href", "static/" + data);
-          $("#download").click(function() {
-            location.reload();
-          });
           console.log(data);
         },
       });
@@ -44,7 +31,6 @@ window.onload = () => {
 };
 
 function readUrl(input) {
-  
   imagebox = $("#imagebox");
   console.log(imagebox);
   console.log("evoked readUrl");
@@ -58,33 +44,5 @@ function readUrl(input) {
       //   imagebox.width(800);
     };
     reader.readAsDataURL(input.files[0]);
-  }
-}
-
-function validasiFile(){
-  var inputFile = document.getElementById('imageinput');
-  var pathFile = inputFile.value;
-  var ekstensiOk = /(\.jpg|\.jpeg|\.png|\.gif|\.mp4|\.avi|\.mov|\.mkv)$/i;
-  if(!ekstensiOk.exec(pathFile)){
-    Swal.fire({
-      title: 'Error',
-      text: "Silakan upload file foto atau video saja",
-      type: 'error',
-    }).then((result) => {
-        if (result.value) {
-          location.reload();
-        }
-    });
-    return false;
-    
-  }else{
-      //Pratinjau gambar
-      if (inputFile.files && inputFile.files[0]) {
-          var reader = new FileReader();
-          reader.onload = function(e) {
-              document.getElementById('pratinjauGambar').innerHTML = '<img src="'+e.target.result+'" width="40%" />';
-          };
-          reader.readAsDataURL(inputFile.files[0]);
-      }
   }
 }
