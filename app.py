9927967ea6841ea5eb916fb1f75abd9f35a2a7b9
aldididi <aldyh024@gmail.com>
2022-07-08 16:04:28 +0700

    change app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ from utils.torch_utils import select_device, time_sync
 # from camera import Detector
 
 @torch.no_grad()
-def model_load(weights="best.pt",  # model.pt path(s)
+def model_load(weights="best2.pt",  # model.pt path(s)
                device='cpu',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
                half=False,  # use FP16 half-precision inference
                dnn=False,  # use OpenCV DNN for ONNX inference
@@ -44,134 +44,142 @@ def model_load(weights="best.pt",  # model.pt path(s)
 
 
 app = Flask(__name__)
-
+# video = cv2.VideoCapture(0)
 uploads_dir = os.path.join(app.instance_path, 'uploads')
 
 os.makedirs(uploads_dir, exist_ok=True)
 
 def gen():
-    model = model_load()
-    device = select_device('cpu')
-    imgsz = [640, 640]  # inference size (pixels)
-    conf_thres = 0.25  # confidence threshold
-    iou_thres = 0.45  # NMS IOU threshold
-    max_det = 1000  # maximum detections per image
-    view_img = False  # show results
-    save_txt = False  # save results to *.txt
-    save_conf = False  # save confidences in --save-txt labels
-    save_crop = False  # save cropped prediction boxes
-    nosave = False  # do not save images/videos
-    classes = None  # filter by class: --class 0, or --class 0 2 3
-    agnostic_nms = False  # class-agnostic NMS
-    augment = False  # ugmented inference
-    visualize = False  # visualize features
-    line_thickness = 3  # bounding box thickness (pixels)
-    hide_labels = False  # hide labels
-    hide_conf = False  # hide confidences
-    half = False  # use FP16 half-precision inference
-    dnn = False  # use OpenCV DNN for ONNX inference
-    source = str(0)
-    webcam = True
-    stride, names, pt, jit, onnx = model.stride, model.names, model.pt, model.jit, model.onnx
-    imgsz = check_img_size(imgsz, s=stride)  # check image size
-    save_img = not nosave and not source.endswith('.txt')  # save inference images
+    capture = cv2.VideoCapture(0)
+    while True:
+        ret, frame = capture.read()
+        if ret == False:
+            continue
+        # encode the frame in JPEG format
+        encodedImage = cv2.imencode(".jpg", frame)
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + bytearray(np.array(encodedImage)) + b'\r\n')
+        model = model_load()
+        device = select_device('cpu')
+        imgsz = [640, 640]  # inference size (pixels)
+        conf_thres = 0.25  # confidence threshold
+        iou_thres = 0.45  # NMS IOU threshold
+        max_det = 1000  # maximum detections per image
+        view_img = False  # show results
+        save_txt = False  # save results to *.txt
+        save_conf = False  # save confidences in --save-txt labels
+        save_crop = False  # save cropped prediction boxes
+        nosave = False  # do not save images/videos
+        classes = None  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms = False  # class-agnostic NMS
+        augment = False  # ugmented inference
+        visualize = False  # visualize features
+        line_thickness = 3  # bounding box thickness (pixels)
+        hide_labels = False  # hide labels
+        hide_conf = False  # hide confidences
+        half = False  # use FP16 half-precision inference
+        dnn = False  # use OpenCV DNN for ONNX inference
+        source = str(0)
+        webcam = True
+        stride, names, pt, jit, onnx = model.stride, model.names, model.pt, model.jit, model.onnx
+        imgsz = check_img_size(imgsz, s=stride)  # check image size
+        save_img = not nosave and not source.endswith('.txt')  # save inference images
 
-    # Dataloader
-    if webcam:
-        view_img = check_imshow()
-        cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt and not jit)
-        bs = len(dataset)  # batch_size
-    else:
-        dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt and not jit)
-        bs = 1  # batch_size
-    vid_path, vid_writer = [None] * bs, [None] * bs
-    # Run inference
-    if pt and device.type != "cpu":
-        model(torch.zeros(1, 3, *imgsz).to(device).type_as(next(model.model.parameters())))  # warmup
-    dt, seen = [0.0, 0.0, 0.0], 0
-    for path, im, im0s, vid_cap, s in dataset:
-        t1 = time_sync()
-        im = torch.from_numpy(im).to(device)
-        im = im.half() if half else im.float()  # uint8 to fp16/32
-        im /= 255  # 0 - 255 to 0.0 - 1.0
-        if len(im.shape) == 3:
-            im = im[None]  # expand for batch dim
-        t2 = time_sync()
-        dt[0] += t2 - t1
-        # Inference
-        # visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
-        pred = model(im, augment=augment, visualize=visualize)
-        t3 = time_sync()
-        dt[1] += t3 - t2
-        # NMS
-        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
-        dt[2] += time_sync() - t3
-        # Second-stage classifier (optional)
-        # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
-        # Process predictions
-        for i, det in enumerate(pred):  # per image
-            seen += 1
-            if webcam:  # batch_size >= 1
-                p, im0, frame = path[i], im0s[i].copy(), dataset.count
-                s += f'{i}: '
-            else:
-                p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
-            p = Path(p)  # to Path
-            # save_path = str(save_dir / p.name) # im.jpg
-            # txt_path = str(save_dir / 'labels' / p.stem) + (
-            # '' if dataset.mode == 'image' else f'_{frame}') # im.txt
-            s += '%gx%g ' % im.shape[2:]  # print string
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-            imc = im0.copy() if save_crop else im0  # for save_crop
-            annotator = Annotator(im0, line_width=line_thickness, example=str(names))
-            if len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+        # Dataloader
+        if webcam:
+            view_img = check_imshow()
+            cudnn.benchmark = True  # set True to speed up constant image size inference
+            dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt and not jit)
+            bs = len(dataset)  # batch_size
+        else:
+            dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt and not jit)
+            bs = 1  # batch_size
+        vid_path, vid_writer = [None] * bs, [None] * bs
+        # Run inference
+        if pt and device.type != "cpu":
+            model(torch.zeros(1, 3, *imgsz).to(device).type_as(next(model.model.parameters())))  # warmup
+        dt, seen = [0.0, 0.0, 0.0], 0
+        for path, im, im0s, vid_cap, s in dataset:
+            t1 = time_sync()
+            im = torch.from_numpy(im).to(device)
+            im = im.half() if half else im.float()  # uint8 to fp16/32
+            im /= 255  # 0 - 255 to 0.0 - 1.0
+            if len(im.shape) == 3:
+                im = im[None]  # expand for batch dim
+            t2 = time_sync()
+            dt[0] += t2 - t1
+            # Inference
+            # visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
+            pred = model(im, augment=augment, visualize=visualize)
+            t3 = time_sync()
+            dt[1] += t3 - t2
+            # NMS
+            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+            dt[2] += time_sync() - t3
+            # Second-stage classifier (optional)
+            # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
+            # Process predictions
+            for i, det in enumerate(pred):  # per image
+                seen += 1
+                if webcam:  # batch_size >= 1
+                    p, im0, frame = path[i], im0s[i].copy(), dataset.count
+                    s += f'{i}: '
+                else:
+                    p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
+                p = Path(p)  # to Path
+                # save_path = str(save_dir / p.name) # im.jpg
+                # txt_path = str(save_dir / 'labels' / p.stem) + (
+                # '' if dataset.mode == 'image' else f'_{frame}') # im.txt
+                s += '%gx%g ' % im.shape[2:]  # print string
+                gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+                imc = im0.copy() if save_crop else im0  # for save_crop
+                annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+                if len(det):
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
 
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                    # Print results
+                    for c in det[:, -1].unique():
+                        n = (det[:, -1] == c).sum()  # detections per class
+                        s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
-                # Write results
-                for *xyxy, conf, cls in reversed(det):
-                    if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(
-                            -1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
-                        # with open(txt_path + '.txt', 'a') as f:
-                        # f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                    # Write results
+                    for *xyxy, conf, cls in reversed(det):
+                        if save_txt:  # Write to file
+                            xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(
+                                -1).tolist()  # normalized xywh
+                            line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+                            # with open(txt_path + '.txt', 'a') as f:
+                            # f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
-                    if save_img or save_crop or view_img:  # Add bbox to image
-                        c = int(cls)  # integer class
-                        label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
-                        annotator.box_label(xyxy, label, color=colors(c, True))
-                        # if save_crop:
-                        # save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg',
-                        # BGR=True)
-            # Print time (inference-only)
-            LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
-            # Stream results
-            im0 = annotator.result()
-            if view_img:
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(1)  # 1 millisecond
-                cv2.destroyAllWindows
+                        if save_img or save_crop or view_img:  # Add bbox to image
+                            c = int(cls)  # integer class
+                            label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+                            annotator.box_label(xyxy, label, color=colors(c, True))
+                            # if save_crop:
+                            # save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg',
+                            # BGR=True)
+                # Print time (inference-only)
+                LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
+                # Stream results
+                im0 = annotator.result()
+                if view_img:
+                    cv2.imshow(str(p), im0)
+                    cv2.waitKey(1)  # 1 millisecond
+                    cv2.destroyAllWindows
 
-        cv2.imwrite('frame.jpg', im0)
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + open('frame.jpg', 'rb').read() + b'\r\n')
-        # String results
-        print(s)
-        # wait key to break
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        ret, jpg = cv2.imencode('.jpg', image)
-        detection = jpg.tobytes()
-        return detection
+            cv2.imwrite('frame.jpg', im0)
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + open('frame.jpg', 'rb').read() + b'\r\n')
+            # String results
+            print(s)
+            # wait key to break
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            ret, jpg = cv2.imencode('.jpg', image)
+            detection = jpg.tobytes()
+            return detection
 
-@app.route('/video_feed')
+@app.route('/video_feed/')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(),
@@ -228,7 +236,7 @@ def return_file():
 # 	return redirect(url_for('static/video_1.mp4', code=200))
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'best.pt', help='model path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'best2.pt', help='model path(s)')
     parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob, 1 for webcam')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.15, help='object confidence threshold')
--- detect.py
+++ detect.py
@@ -24,7 +24,7 @@ from utils.torch_utils import select_device, time_sync
 
 @torch.no_grad()
 def run(
-        weights=ROOT / 'best.pt',  # model.pt path(s)
+        weights=ROOT / 'best2.pt',  # model.pt path(s)
         source=ROOT / 'data/images',  
         data=ROOT / 'data/coba.yaml',  # dataset.yaml path
         imgsz=(640, 640),
@@ -186,7 +186,7 @@ def run(
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'best.pt', help='model path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'best2.pt', help='model path(s)')
     parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob, 0 for webcam')
     parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
